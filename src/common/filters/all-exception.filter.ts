d9923f8afa40dd983406c4ae0744d795c953b743
NH-99-FE <lianglh_only@163.com>
2025-06-06 11:19:55 +0800

    全局设置：开启filters、CORS、API请求前缀+版本

---
+++ src/common/filters/all-exception.filter.ts
@@ -0,0 +1,39 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import * as requestIp from 'request-ip'
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger()
+  constructor(private readonly httpAdapterHost: HttpAdapterHost){ }
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+    
+    const msg: unknown = (exception as any)?.response || 'Internal Server Error';
+
+    // 这里可以加入更多异常处理逻辑
+    const responseBody = {
+      headers: request.headers,
+      query: request.query,
+      body: request.body,
+      params: request.params,
+      timestamp: new Date().toISOString(),
+      // 还可以加入一些用户信息
+      // IP信息
+      ip: requestIp.getClientIp(request),
+      exceptioin: (exception as any)?.name,
+      error: msg,
+    };
+
+    this.logger.error('[all-exception]', responseBody);
+    httpAdapter.reply(response, responseBody, httpStatus);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,45 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
-
   const configService = app.get(ConfigService)
   const port = configService.get<number>('PORT', 3000)
+  const errorFilterFlag = configService.get<string>('ERROR_FILTER')
+  const cors = configService.get('CORS', false)
+  const prefix = configService.get('PREFIX', '/api')
+  const versionStr = configService.get<string>('VERSION');
+  let version: string | string[] | typeof VERSION_NEUTRAL;
+
+  if (!versionStr) {
+    version = VERSION_NEUTRAL;
+  } else if (versionStr.includes(',')) {
+    version = versionStr.split(',').filter(Boolean);
+  } else {
+    version = versionStr;
+  }
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
+  app.setGlobalPrefix(prefix)
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: version,
+  })
+
+  if (cors === 'true'){
+    app.enableCors();
+  }
+
+  if (errorFilterFlag === 'true'){
+  
+    const httpAdapter = app.get(HttpAdapterHost)
+    app.useGlobalFilters(new AllExceptionFilter(httpAdapter))
+  }
+
   await app.listen(port);
 }
 bootstrap();
